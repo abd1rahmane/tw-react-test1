4f49be5279abd191df3b05121f861d3fe73f6490
AbdeRahmane Messaadi <abderrahmane8@yandex.com>
2023-05-20 16:27:55 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// Dependencies
+import React from "react";
+
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-700">
+      <h2 className="text-center text-4xl font-bold text-blue-200">
+        TailwindCSS + React
+      </h2>
+    </div>
+  );
+};
+
+export default App;
